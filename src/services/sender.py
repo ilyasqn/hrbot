ab98375d7547ba8372d1da4e5a083bfd07883496
Ilyas <ilyas.shamshiden@mail.ru>
2025-08-02 05:10:00 +0500

    add temp send to back function

--- src/services/sender.py
+++ src/services/sender.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import aiohttp
 from aiogram import Bot
 from src.configs.bot import bot_settings
@@ -12,17 +14,22 @@ class Sender:
     async def send_vacancy(cls, data: dict):
         title = "📝 Новая вакансия"
         text = await Formatter.format_vacancy(data)
-        await cls._send_to_channel(title, text, data)
+        await cls._send_to_channel(title, text)
+        # await cls._send_to_backend(data, prefix='vacancy')
 
     @classmethod
     async def send_event(cls, data: dict):
         title = "📣 Новое мероприятие"
         text = await Formatter.format_event(data)
-        await cls._send_to_channel(title, text, data)
+        await cls._send_to_channel(title, text)
+        # await cls._send_to_backend(data, prefix='event')
 
     @classmethod
-    async def _send_to_channel(cls, title: str, text: str, data: dict):
+    async def _send_to_channel(cls, title: str, text: str):
         text = f"{title}\n\n{text}"
         await cls.bot.send_message(chat_id=bot_settings.CHANNEL_ID, text=text)
-        # async with aiohttp.ClientSession() as session:
-        #     await session.post(backend_settings.URL, json=data)
+
+    @classmethod
+    async def _send_to_backend(cls, data: dict, prefix: str):
+        async with aiohttp.ClientSession() as session:
+            await session.post(f'{backend_settings.URL}/{prefix}', json=data) # temp realization
